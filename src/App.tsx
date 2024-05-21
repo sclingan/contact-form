86474b3a4e4598aaac2b96d8c055fb68b532b292
Sclingan <sclingan1@gmail.com>
2024-05-21 13:05:47 -0500

    Adding basic mobile layout and css variables

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,34 @@ function App() {
       <h1>Contact Us</h1>
       <form>
         <label htmlFor='name'>First Name</label>
-        <input type='text' name='name'></input>
-        <p className='error'>This field is required</p>
+        <input type='text' name='name' id='name' autoComplete='given-name'></input>
+        <p className='sr-only'>This field is required</p>
         <label htmlFor='Lname'>Last Name</label>
-        <input type='text' name='Lname'></input>
-        <p className='error'>This field is required</p>
+        <input type='text' name='Lname' id='Lname' autoComplete='family-name'></input>
+        <p className='sr-only'>This field is required</p>
         <label htmlFor='email'>Email Address</label>
-        <input type='email' name='email'></input>
-        <p className='formatError'>Please enter a valid email address</p>
-        <p className='error'>This field is required</p>
+        <input type='email' name='email' id='email' autoComplete='email'></input>
+        <p className='sr-only'>Please enter a valid email address</p>
+        <p className='sr-only'>This field is required</p>
         <fieldset>
           <legend>Query Type</legend>
+          <div className='radio'>
           <input type='radio' id='general' name='query' value='General Enquiry'></input>
           <label htmlFor='general'>General Enquiry</label>
+          </div>
+          <div className='radio'>
           <input type='radio' id='support' name='query' value='Support Request'></input>
           <label htmlFor='support'>Support Request</label>
-          <p className='error'>Please select a query type</p>
+          </div>
+          <p className='sr-only'>Please select a query type</p>
         </fieldset>
         <label htmlFor='message'>Message</label>
-        <input type='textarea' name='message'></input>
-        <p className='error'>This field is required</p>
+        {/* <input type='textarea' name='message' id='message' autoComplete='off'></input> */}
+        <textarea id='message' name='message'></textarea>
+        <p className='sr-only'>This field is required</p>
         <input type='checkbox' id='consent' name='consent'></input>
         <label htmlFor='consent'>I consent to being contacted by the team</label>
-        <p className='error'>To submit this form, please consent to being contacted</p>
+        <p className='sr-only'>To submit this form, please consent to being contacted</p>
         <button className='submit'>Submit</button>
       </form>
     </main>
