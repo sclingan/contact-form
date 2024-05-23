240b1c3b8aa54c478b8d863cc4359b30e9a6e2ea
Sclingan <sclingan1@gmail.com>
2024-05-23 13:00:51 -0500

    Adding active states

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,29 @@ function App() {
     <main>
       <h1>Contact Us</h1>
       <form>
-        <label htmlFor='name'>First Name</label>
+        <label htmlFor='name'>First Name <span>*</span></label>
         <input type='text' name='name' id='name' autoComplete='given-name'></input>
         <p className='sr-only'>This field is required</p>
-        <label htmlFor='Lname'>Last Name</label>
+        <label htmlFor='Lname' id='labelLname'>Last Name <span>*</span></label>
         <input type='text' name='Lname' id='Lname' autoComplete='family-name'></input>
         <p className='sr-only'>This field is required</p>
-        <label htmlFor='email'>Email Address</label>
+        <label htmlFor='email'>Email Address *</label>
         <input type='email' name='email' id='email' autoComplete='email'></input>
         <p className='sr-only'>Please enter a valid email address</p>
         <p className='sr-only'>This field is required</p>
         <fieldset>
-          <legend>Query Type</legend>
+          <legend>Query Type <span>*</span></legend>
           <div className='radio'>
           <input type='radio' id='general' name='query' value='General Enquiry'></input>
           <label htmlFor='general'>General Enquiry</label>
           </div>
-          <div className='radio'>
+          <div className='radio' id='div_support'>
           <input type='radio' id='support' name='query' value='Support Request'></input>
           <label htmlFor='support'>Support Request</label>
           </div>
           <p className='sr-only'>Please select a query type</p>
         </fieldset>
-        <label htmlFor='message'>Message</label>
+        <label htmlFor='message'>Message <span>*</span></label>
         <textarea id='message' name='message' cols={10} rows={10}></textarea>
         <p className='sr-only'>This field is required</p>
         <div className='checkbox'>
