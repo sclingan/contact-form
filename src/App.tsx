e16e9a80758321a85b0bb3cb6c9e81f5e4ba3d1b
Sclingan <sclingan1@gmail.com>
2024-05-27 23:03:58 -0500

    Adding form validation

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,32 @@ function App() {
   const [consent, setConsent] = useState('');
 
   function handleChange(e) {
-    setName(e.target.value);
-    console.log(name);
+    const input = e.target.id;
+    const value = e.target.value;
+    switch(input) {
+      case 'name':
+        setName(value);
+        break;
+      case 'Lname':
+        setLname(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'query':
+        setQuery(value);
+        break;
+      case 'message':
+        setMessage(value);
+        break;
+      case 'consent':
+        setConsent(value);
+        break;
+      default:
+        console.log('error');
+    }
+    console.log(name, Lname, email, query, message, consent);
+
   }
 
  
@@ -26,29 +50,29 @@ function App() {
         <input type='text' name='name' id='name' autoComplete='given-name' onChange={handleChange}></input>
         <p className='vis' id='name_error'>This field is required</p>
         <label htmlFor='Lname' id='labelLname'>Last Name <span>*</span></label>
-        <input type='text' name='Lname' id='Lname' autoComplete='family-name'></input>
+        <input type='text' name='Lname' id='Lname' autoComplete='family-name' onChange={handleChange}></input>
         <p className='vis' id='Lname_error'>This field is required</p>
         <label htmlFor='email'>Email Address *</label>
-        <input type='email' name='email' id='email' autoComplete='email'></input>
+        <input type='email' name='email' id='email' autoComplete='email' onChange={handleChange}></input>
         <p className='formatError'>Please enter a valid email address</p>
         <p className='vis' id='email_error'>This field is required</p>
         <fieldset>
           <legend>Query Type <span>*</span></legend>
           <div className='radio'>
-          <input type='radio' id='general' name='query' value='General Enquiry'></input>
+          <input type='radio' id='general' name='query' value='General Enquiry' onChange={handleChange}></input>
           <label htmlFor='general'>General Enquiry</label>
           </div>
           <div className='radio' id='div_support'>
-          <input type='radio' id='support' name='query' value='Support Request'></input>
+          <input type='radio' id='support' name='query' value='Support Request' onChange={handleChange}></input>
           <label htmlFor='support'>Support Request</label>
           </div>
           <p className='vis' id='query_error'>Please select a query type</p>
         </fieldset>
         <label htmlFor='message'>Message <span>*</span></label>
-        <textarea id='message' name='message' cols={10} rows={10}></textarea>
+        <textarea id='message' name='message' cols={10} rows={10} onChange={handleChange}></textarea>
         <p className='vis' id='message_error'>This field is required</p>
         <div className='checkbox'>
-        <input type='checkbox' id='consent' name='consent'></input>
+        <input type='checkbox' id='consent' name='consent' onChange={handleChange}></input>
         <label htmlFor='consent'>I consent to being contacted by the team</label>
         </div>
         <p className='vis' id='consent_error'>To submit this form, please consent to being contacted</p>
